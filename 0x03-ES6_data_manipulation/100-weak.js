3889e83e01690e76ebb0094b4e6966c97b882e89
johnny <johnametepeagboku@gmail.com>
2024-06-05 01:59:30 +0000

    Update: Task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+// Exporting a const instance of WeakMap
+export const weakMap = new WeakMap();
+
+// Exporting the queryAPI function
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const queryCount = weakMap.get(endpoint) + 1;
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, queryCount);
+}
