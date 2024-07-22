748de273cc43f41551a7a8bbe8e135b1b3cf1135
johnny <johnametepeagboku@gmail.com>
2024-07-22 13:40:54 +0000

    Update: Task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+export default async function countStudents(path) {
+  try {
+    const data = await fs.promises.readFile(path, 'utf8');
+    const lines = data.split('\n').filter(Boolean); // Remove empty lines
+
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    let isFirstLine = true;
+    let numStudents = 0;
+    let numCS = 0;
+    let numSWE = 0;
+    const csStudents = [];
+    const sweStudents = [];
+
+    lines.forEach((line) => {
+      if (isFirstLine) {
+        isFirstLine = false; // Skip the first line (header)
+      } else {
+        const [firstname, , , field] = line.split(',');
+
+        if (field === 'CS') {
+          numCS += 1;
+          csStudents.push(firstname);
+        } else if (field === 'SWE') {
+          numSWE += 1;
+          sweStudents.push(firstname);
+        }
+
+        numStudents += 1;
+      }
+    });
+
+    console.log(`Number of students: ${numStudents}`);
+    console.log(`Number of students in CS: ${numCS}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${numSWE}. List: ${sweStudents.join(', ')}`);
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
+}
