a551538a1cf0b4391873407c98aef50f48b5d257
johnny <johnametepeagboku@gmail.com>
2024-07-22 13:36:07 +0000

    Update: Task 2

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const process = require("process");
+
+process.stdout.write("Welcome to Holberton School; what is your name?");
+process.stdout.write("\n");
+
+process.stdin.on("readable", function() {
+    const chunk = process.stdin.read();
+
+    if (chunk !== null) {
+        process.stdout.write(`Your name is ${chunk}`);
+    }
+})
+
+
+process.on('exit', () => {
+    process.stdout.write("This important software is now closing\n");
+})
